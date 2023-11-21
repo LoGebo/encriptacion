013ec96388070b0a03dd25b5db7878ba68782888
Jesús Daniel Martínez <90727612+LoGebo@users.noreply.github.com>
2023-11-21 16:54:07 -0600

    Versión mejorada

--- README.md
+++ README.md
@@ -1,4 +1,54 @@
 # encriptacion
 
-#instalar libreria de criptografia: pip install cryptography
-#ejecutar el programa: python gfgimproved.py o python3 gfgimproved.py según corresponda.
+# Encriptación
+
+## Instalación de la librería de criptografía
+
+Para instalar la librería de criptografía, corra lo siguiente:
+
+pip install cryptography
+
+
+## ejecutar el programa: python script.py encrypt <path-de-la-imagen>
+
+
+
+# Para desencriptar: python script.py decrypt <path-de-la-imagen> --key <tu-clave>
+
+
+### Notas:
+
+El script verifica si el archivo proporcionado es una imagen válida antes de realizar la encriptación.
+La clave de encriptación se genera de manera aleatoria para cada encriptación, lo que mejora la seguridad en comparación con el uso de una clave fija.
+El script guarda la imagen encriptada en un nuevo archivo para no sobrescribir la imagen original.
+Se manejan posibles errores y se proporcionan mensajes de error informativos en caso de problemas.
+AES ofrece seguridad adicional para verificar que los datos no se hayan alterado durante la transferencia o almacenamiento.
+
+
+# Encriptación de Imágenes
+Verifica si el archivo proporcionado es una imagen.
+Genera una clave y un vector de inicialización (IV) aleatorios.
+Abre el archivo de la imagen y lee sus datos.
+Crea un objeto Cipher con el algoritmo AES y el modo CFB utilizando la clave y el IV.
+Cifra los datos de la imagen utilizando el encryptor del objeto Cipher.
+Guarda el IV al inicio del archivo cifrado y luego los datos cifrados en un nuevo archivo con la extensión ".enc".
+
+
+# Desencriptación de Imágenes
+Lee el archivo cifrado que se desea desencriptar.
+Lee los primeros 16 bytes del archivo para obtener el IV.
+Crea un objeto Cipher con el algoritmo AES y el modo CFB utilizando la clave de desencriptación proporcionada en formato hexadecimal y el IV.
+Descifra los datos del archivo utilizando el decryptor del objeto Cipher.
+Determina la extensión original de la imagen.
+Guarda la imagen desencriptada en un nuevo archivo con el nombre y la extensión originales.
+
+Básicamente Utiliza el algoritmo AES, que es altamente confiable, y genera claves y Initialization Vectors (IV) aleatorios para cada operación. Además, maneja errores y almacena las claves de desencriptación de forma segura. 
+
+"Advanced Encryption Standard (AES) es uno de los algoritmos de cifrado más utilizados y seguros actualmente disponibles. Es de acceso público, y es el cifrado que la NSA utiliza para asegurar documentos con la clasificación "top secret". Su historia de éxito se inició en 1997, cuando el NIST (Instituto Nacional de Estándares y Tecnología) comenzó oficialmente a buscar un sucesor al envejecimiento cifrado estándar DES. Un algoritmo llamado "Rijndael", desarrollado por los criptografistas belgas Daemen y Rijmen, sobresalía tanto en seguridad como en rendimiento y flexibilidad."
+
+Referencias:
+# https://cryptography.io/en/latest/hazmat/primitives/symmetric-encryption/ 
+# https://www.boxcryptor.com/es/encryption/ 
+
+
+
--- gfgimproved.py
+++ gfgimproved.py
@@ -1,38 +1,89 @@
+import argparse
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
 import os
 
-try:
-    # Ruta del archivo de imagen
-    path = "11.jpg"   # aqui ponga el path profe
+def encrypt_image(file_path):
+    # Verificar si el archivo es una imagen o nelson
+    if not file_path.lower().endswith(('.png', '.jpg', '.jpeg', '.bmp', '.gif')):
+        raise ValueError("El archivo proporcionado no es una imagen")
 
-    # Clave de cifrado generada de manera segura
-    key = os.urandom(32)  # Clave AES de 256 bits (basada)
-    iv = os.urandom(16)    
-    # Imprimir la ruta del archivo y la clave de cifrado
-    print('The path of file : ', path)
-    print('Key for encryption : ', key)
-    print("el iv papu", iv)
+    # Generar clave y IV SIUUUUUUUUUU
+    key = os.urandom(32) 
+    iv = os.urandom(16)   
 
-    # Leer los datos de la imagen
-    with open(path, 'rb') as fin:
-        image_data = fin.read()
+    try:
 
-    # Cifrar la imagen usando AES
-    cipher = Cipher(algorithms.AES(key), modes.CFB(os.urandom(16)), backend=default_backend())
-    encryptor = cipher.encryptor()
-    encrypted_image = encryptor.update(image_data) + encryptor.finalize()
+        with open(file_path, 'rb') as fin:
+            image_data = fin.read()
 
-    # Guardar la imagen cifrada en un nuevo archivo siuuuuuuuu
-    encrypted_path = path + ".enc"
-    with open(encrypted_path, 'wb') as fout:
-        fout.write(iv)  # Escribe el IV al comienzo del archivo pa poderlo buscar despues
-        fout.write(encrypted_image)
+        # Cifrar la imagen usando AEs (GOD)
+        cipher = Cipher(algorithms.AES(key), modes.CFB(iv))
+        encryptor = cipher.encryptor()
+        encrypted_image = encryptor.update(image_data) + encryptor.finalize()
 
-    print('Encryption Done. Encrypted file:', encrypted_path)
+        # Guardar la imagen cifrada en un nuevo arc
+        encrypted_path = file_path + ".enc"
+        with open(encrypted_path, 'wb') as fout:
+            fout.write(iv)  # Guardar IV al inicio del archivo papu
+            fout.write(encrypted_image)
 
-except Exception as e:
-    print('Error caught:', str(e))
+        print('Encriptación completada. Archivo encriptado:', encrypted_path)
+        print('Clave de encriptación:', key.hex())
+    except Exception as e:
+        print('Error:', str(e))
+
+# Uso del script
+# encrypt_image("ruta/a/tu/imagen.jpg")
+
+def decrypt_image(encrypted_file_path, key):
+    try:
+        # Leer los datos encriptados de la img
+        with open(encrypted_file_path, 'rb') as fin:
+            iv = fin.read(16)  # Leer los primeros 16 bytes para el IV que se generó alla arriba dx
+            encrypted_data = fin.read()
+
+        # Descifrar la imagen siuuu 
+        cipher = Cipher(algorithms.AES(bytes.fromhex(key)), modes.CFB(iv))
+        decryptor = cipher.decryptor()
+        decrypted_image = decryptor.update(encrypted_data) + decryptor.finalize()
+
+
+        original_extension = encrypted_file_path.split('.')[-2].split('/')[-1]
+        # verif la exntesion de la img
+        if original_extension not in ['jpg', 'jpeg', 'png', 'bmp', 'gif']:
+            raise ValueError("La extensión del archivo original no es un formato de imagen reconocido")
+
+        # Guardar la imagen desencriptada siuuu
+        decrypted_path = encrypted_file_path.replace('.enc', f'_decrypted.{original_extension}')
+        with open(decrypted_path, 'wb') as fout:
+            fout.write(decrypted_image)
+
+        print('Desencriptación completada. Archivo desencriptado:', decrypted_path)
+    except Exception as e:
+        print('Error:', str(e))
+
+
+# Uso del script
+# decrypt_image("ruta/al/archivo/encriptado.enc", "tu_clave_en_hexadecimal")
+
+def main():
+    parser = argparse.ArgumentParser(description="Encriptar y desencriptar imágenes usando AES")
+    parser.add_argument("action", choices=["encrypt", "decrypt"], help="acción a realizar: 'encrypt' o 'decrypt'")
+    parser.add_argument("file_path", help="ruta al archivo a procesar")
+    parser.add_argument("--key", help="clave de encriptación para desencriptar, en formato hexadecimal", default="")
+
+    args = parser.parse_args()
+
+    if args.action == "encrypt":
+        encrypt_image(args.file_path)
+    elif args.action == "decrypt":
+        if not args.key:
+            raise ValueError("Se requiere una clave para desencriptar")
+        decrypt_image(args.file_path, args.key)
+
+if __name__ == "__main__":
+    main()
+
 
 
 #guia de uso 
@@ -40,16 +91,18 @@ except Exception as e:
 #ejecutar el programa: python gfgimproved.py o python3 gfgimproved.py según corresponda.
 
 # ¿Por qué este código es más chidote que el anterior (partiendo de la base del gfg)?
-# 1. AES es como un guardia de seguridad de nivel ninja en comparación con el XOR, que sería un candado básico. AES es bastante seguro.
+# 1. AES es como un guardia de seguridad mas chidori en comparación con el XOR, que sería un candado básico. AES es bastante seguro.
 # 2. Las claves que usamos aquí son generadas al azar, lo cual es mucho mejor que usar una clave fija como "GeeksforGeeks". practicamente Es como tener una contraseña única para cada candado.
 # 3. Este código guarda la imagen cifrada en un nuevo archivo en vez de sobrescribir el original pa no perder la foto original
 # 4. manejamos los posibles errors.
-# 5. Aunque no lo implementamos completamente aquí, AES nos permite agregar más seguridad para chequear que nuestros datos no se hayan alterado.
+# 5. Aunque no lo implementamos compleztamente aquí, AES nos permite agregar más seguridad para chequear que nuestros datos no se hayan alterado.
 # 6. Usar AES es estar basado en seguridad (creo). 
 
-# En resumen, este código es más seguro y confiable para encriptar nuestras imágenes.
+# En resumen, este código es más seguro y confiable para encriptar nuestras imágenes partiendo de la misma base.
 
 #JESUS DANIEL MARTÍNEZ GARCÍA A00833591
 
 # Referencias: Geeks for geeks encrypt and decrypt an image
 #https://www.geeksforgeeks.org/encrypt-and-decrypt-image-using-python/
+
+#https://cryptography.io/en/latest/hazmat/primitives/symmetric-encryption/ 
